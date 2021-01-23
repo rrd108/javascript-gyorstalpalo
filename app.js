212ef6273dce0b8b29c82bd3a0c59ef373cea0d0
rrd <rrd@1108.cc>
2021-01-23 09:48:47 +0100

    kommentek

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const vegburger = document.getElementById('vegburger')
 const nav = document.getElementById('nav')
 
+// klikkre css osztályokat cserélünk
 vegburger.addEventListener('click', function (event) {
   nav.classList.toggle('menu-active')
   vegburger.classList.toggle('fi-align-justify')
   vegburger.classList.toggle('fi-arrow-left')
 })
+
+// Modern javascript - plain / vanilla js
+// 2005 jQuery $('#vegburger')
+// keretrendszerek - vue, react, angular

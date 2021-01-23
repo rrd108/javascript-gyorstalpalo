8b034783a715e63e2467039e196e27682374eaa6
rrd <rrd@1108.cc>
2021-01-23 09:48:36 +0100

    vegburger icon csere

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ const nav = document.getElementById('nav')
 
 vegburger.addEventListener('click', function (event) {
   nav.classList.toggle('menu-active')
+  vegburger.classList.toggle('fi-align-justify')
+  vegburger.classList.toggle('fi-arrow-left')
 })

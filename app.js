6b47df4ff2c9e07ac024a5b4bedbb932f9af5f72
rrd <rrd@1108.cc>
2021-01-23 16:23:57 +0100

    termékek beszúrása

--- app.js
+++ app.js
@@ -8,6 +8,42 @@ vegburger.addEventListener('click', function (event) {
   vegburger.classList.toggle('fi-arrow-left')
 })
 
-// Modern javascript - plain / vanilla js
-// 2005 jQuery $('#vegburger')
-// keretrendszerek - vue, react, angular
+// ez általában egy backendről jön
+const products = [
+  {
+    name: 'Málna',
+    picture: 'malna.jpg',
+    description: 'Kézzel termelt egészség',
+    price: 3800,
+    inStock: true
+  },
+  {
+    name: 'Áfonya',
+    picture: 'afonya.jpg',
+    description: 'Kézzel termelt egészség',
+    price: 3250,
+    inStock: true
+  },
+  {
+    name: 'Szeder',
+    picture: 'szeder.jpg',
+    description: 'Kézzel termelt egészség',
+    price: 1700,
+    inStock: true,
+    variations: ['fehér', 'fekete']
+  }
+]
+
+const productsSection = document.getElementById('products')
+
+// TODO inStock, variations
+
+products.forEach(product => {
+  productsSection.innerHTML += `<div>
+    <h2>${product.name}</h2>
+    <p>${product.description}</p>
+    <img src="./img/${product.picture}">
+    <h3>${product.price} Ft</h3>
+    <a href="#">Kosárba</a>
+  </div>`
+})

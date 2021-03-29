78beb05705520493c24e4b1e12c6688457a93ef0
rrd <rrd@1108.cc>
2021-03-29 11:51:39 +0200

    1. rész - tömb függvények

--- app.js
+++ app.js
@@ -1,140 +1,42 @@
-// oldalsó menü kezelése *************************************************
-const vegburger = document.getElementById('vegburger')
-const nav = document.getElementById('nav')
+const products = require('./data.js')
 
-console.log(this)
+console.log('All products', products)
 
-// klikkre css osztályokat cserélünk
-vegburger.addEventListener('click', function () {
-  nav.classList.toggle('menu-active')
-  vegburger.classList.toggle('fi-align-justify')
-  vegburger.classList.toggle('fi-arrow-left')
-  console.log(this)
-})
+// length
+console.log('Tömb méret: ', products.length)
 
-nav.addEventListener('mouseleave', () => {
-  nav.classList.remove('menu-active')
-  vegburger.classList.remove('fi-arrow-left')
-  vegburger.classList.add('fi-align-justify')
-  console.log(this)
-})
+// push
+console.log('új eleme beszúrása', products.push({ id: 100, name: 'Alma' }))
+console.log(products)
 
-// termékek beillesztése *************************************************
-let products = []
-const productsSection = document.getElementById('products')
+// find
+console.log('Find: Eper', products.find(product => product.name == 'Eper'))
+console.log('Find: 10 darabos', products.find(product => product.stock == 10))
 
-fetch('https://hur.webmania.cc/products.json')
-  .then(response => response.json())
-  .then(data => {
-    console.log(this)
-    products = data.products
-    products.forEach(product => {
-      productsSection.innerHTML += `<div>
-        <h2>${product.name}</h2>
-        <p>${product.description}</p>
-        <img src="${product.picture}">
-        <h3>${product.price} Ft</h3>`
-      if (product.stock) {
-        productsSection.innerHTML += `<a id="${product.id}" class="addToCart">Kosárba</a>`
-      } else {
-        productsSection.innerHTML += 'Nem rendelhető'
-      }
-      productsSection.innerHTML += `</div>`
+// findIndex
+console.log('FindIndex: Áfonya', products.findIndex(product => product.name == 'Áfonya'))
 
-      // gyűjtsük ki az addToCart css class-ú elemeket
-      const addToCartButtons = document.getElementsByClassName('addToCart')
-      // nézzük meg, hogy hány darab van belőle
-      const buttonCount = addToCartButtons.length
-      // lépegessünk végig rajta
-      for (let i = 0; i < buttonCount; i++) {
-        // adjunk hozzájuk egyesével egy click figyelőt
-        addToCartButtons[i].addEventListener('click', addToCart)
-      }
-    })
-  })
-  .catch(error => console.error(error))
+// filter
+console.log('Filter: összes 10 darabos', products.filter(product => product.stock == 10))
+console.log('Filter: összes kevesebb mint 100 darabos', products.filter(product => product.stock < 20))
 
-// kosár kezelése *************************************************
-const cart = {}
+// forEach
+products.forEach(product => console.log(product.name))
 
-const addToCart = (event) => {
-  // nézzük meg, hogy a kosárba gombot nyomtuk meg (van id), vagy a plusz gombot (nincs id)
+// map
+console.log(products.map(product => ({id: product.id, name: product.name })))
 
-  /*let target = event.target.dataset.id
-  if (event.target.id) {
-    target = event.target.id
-  }*/
+// some
+console.log('Some: van 10 darabos?', products.some(product => product.stock == 10))
 
-  // ternary operator
-  let target = event.target.id ? event.target.id : event.target.dataset.id
+// sort
+console.log('Stock szerint sorba rendezve: ', products.sort((a, b) => a.stock - b.stock))
+console.log('Id szerint csökkenő sorba rendezve: ', products.sort((a, b) => b.id - a.id))
+console.log('Name szerint csökkenő sorba rendezve: ', products.sort((a, b) => {
+  if (a.name < b.name) { return -1 }
+  if (a.name > b.name) { return 1 }
+  return 0
+}))
 
-  // ha még nincs benne a kosárban akkor adjuk hozzá 1 darabbal
-  if (cart[target] == undefined) {
-    cart[target] = 1
-  } else {
-    // ha már benne van akkor növeljük a darab számot
-    cart[target]++
-  }
-}
+console.log('Unicode rendezve nevek szerint', products.sort((a, b) => a.name.localeCompare(b.name, 'hu-HU')))
 
-const discountMinimumAmount = 30000
-const discountMinimumPieces = 12
-const discount = 0.1
-
-const refreshCartItems = () => {
-  // jelenlegi cart-items tartalom kiürítése
-  cartItems.innerHTML = ''
-  // total 0-ázása
-  let total = 0, maxPieces = 0
-  // lépegessünk végig a cart-on és products tömbből keressük ki a szóban forgó terméket
-  for (const id in cart) {
-    // és jelenítsük meg a nevét, a cartban lévő darabszámot, és a termék árát
-    const currentProduct = products.find(product => product.id == id)
-    cartItems.innerHTML += `<li>
-      <button data-id="${currentProduct.id}">+</button>
-      ${cart[id]} db - ${currentProduct.name} * ${currentProduct.price} Ft/db
-      </li>`
-    // adjuk hozzá ennek az értékét a teljes összeghez
-    total += currentProduct.price * cart[id]
-
-    maxPieces = cart[id] > maxPieces ? cart[id] : maxPieces
-  }
-
-  // ha van olyan termék amiből több mint 10 van vagy a total > 50000 akkor adjunk 10% kedvezményt
-  /*logikai vizsgálatok
-  egyenlő: ==
-  típusosan egyenlő: ===
-  vagy: ||
-  és: &&
-  nem: ! pl (birthday != today)
-  */
-  // TODO magic numbers
-  if (total > discountMinimumAmount || maxPieces >= discountMinimumPieces) {
-    cartItems.innerHTML += `<li>Kedvezmény: ${(total * discount).toLocaleString()} Ft</li>`
-  }
-
-  // a végén jelenítsük meg a teljes vásárlási összeget
-  // TODO az összesenből vonjuk le a kedvezményt ha van
-  cartItems.innerHTML += `<li>Összesen: ${total.toLocaleString()} Ft</li>`
-
-}
-
-const cartIcon = document.getElementById('cart-icon')
-const cartContent = document.getElementById('cart-content')
-const cartItems = document.getElementById('cart-items')
-
-// tegyünk rá egy click figyelőt a kosár iconra
-cartIcon.addEventListener('click', function () {
-  // jelenítsük meg ami a kosárban van
-  cartContent.classList.toggle('active')
-  refreshCartItems()
-  console.log(this)
-})
-
-// tegyük rá a "+" gombokra a click figyelőt a kosárba helyezéssel
-// event delegeation
-//cartItems.addEventListener('click', addToCart)
-cartItems.addEventListener('click', (event) => {
-  addToCart(event)
-  refreshCartItems()
-})
---
+++ data.js
@@ -0,0 +1,112 @@
+const products = [
+  {
+    "id": 21,
+    "category": "Bogyós",
+    "name": "Málna",
+    "description": "Kézzel termelt egészség",
+    "picture": "https://hur.webmania.cc/img/malna.jpg",
+    "price": 3800,
+    "stock": 500
+  },
+  {
+    "id": 17,
+    "category": "Bogyós",
+    "name": "Áfonya",
+    "description": "Az erdő kincse az otthonodba",
+    "picture": "https://hur.webmania.cc/img/afonya.jpg",
+    "price": 3250,
+    "stock": 120
+  },
+  {
+    "id": 3,
+    "category": "Bogyós",
+    "name": "Szeder",
+    "description": "A hagyományos csemege",
+    "picture": "https://hur.webmania.cc/img/szeder.jpg",
+    "price": 1700,
+    "stock": 40
+  },
+  {
+    "id": 14,
+    "category": "Bogyós",
+    "name": "Eper",
+    "description": "Egy tavaszi harapás",
+    "picture": "https://hur.webmania.cc/img/eper.jpg",
+    "price": 1440,
+    "stock": 0
+  },
+  {
+    "id": 5,
+    "category": "Bogyós",
+    "name": "Homoktövis",
+    "description": "Mezei csemege",
+    "picture": "https://hur.webmania.cc/img/homoktovis.jpg",
+    "price": 3200,
+    "stock": 10
+  },
+  {
+    "id": 16,
+    "category": "Bogyós",
+    "name": "Som",
+    "description": "A fanyar gyönyör",
+    "picture": "https://hur.webmania.cc/img/som.jpg",
+    "price": 900,
+    "stock": 10
+  },
+  {
+    "id": 7,
+    "category": "Bogyós",
+    "name": "Fanyarka",
+    "description": "Édes mint a méz",
+    "picture": "https://hur.webmania.cc/img/fanyarka.jpg",
+    "price": 990,
+    "stock": 25
+  },
+  {
+    "id": 8,
+    "category": "Bogyós",
+    "name": "Piszke",
+    "description": "Egres",
+    "picture": "https://hur.webmania.cc/img/piszke.jpg",
+    "price": 750,
+    "stock": 100
+  },
+  {
+    "id": 19,
+    "category": "Bogyós",
+    "name": "Ribizli",
+    "description": "Fanyar, vasban gazdag",
+    "picture": "https://hur.webmania.cc/img/ribizli.jpg",
+    "price": 1300,
+    "stock": 170
+  },
+  {
+    "id": 10,
+    "category": "Magyaros",
+    "name": "Meggy",
+    "description": "A falusi kincs",
+    "picture": "https://hur.webmania.cc/img/meggy.jpg",
+    "price": 600,
+    "stock": 300
+  },
+  {
+    "id": 11,
+    "category": "Magyaros",
+    "name": "Cseresznye",
+    "description": "A falusi kincs",
+    "picture": "https://hur.webmania.cc/img/cseresznye.jpg",
+    "price": 900,
+    "stock": 300
+  },
+  {
+    "id": 12,
+    "category": "Magyaros",
+    "name": "Szilva",
+    "description": "A falusi kincs",
+    "picture": "https://hur.webmania.cc/img/szilva.jpg",
+    "price": 770,
+    "stock": 200
+  }
+]
+
+module.exports = products
